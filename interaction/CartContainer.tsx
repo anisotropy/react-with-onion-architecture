ba0f34831c9c8269b448130eeeec2db2fb6dde3b
anisotropy <j.hodoug@aroundus.com>
2022-06-25 18:50:47 +0900

    Show purchase error message

--- interaction/CartContainer.tsx
+++ interaction/CartContainer.tsx
@@ -3,6 +3,7 @@ import Cart from "./components/Cart";
 import CartLayer from "./components/CartLayer";
 import CartAfterPurchase from "./components/CartAfterPurchase";
 import usePurchase from "./functions/usePurchase";
+import PurchaseError from "./components/PurchaseError";
 
 export default function CartContainer() {
   const cartItems = useCartState();
@@ -11,7 +12,9 @@ export default function CartContainer() {
 
   return (
     <CartLayer>
-      {purchase.items ? (
+      {purchase.error ? (
+        <PurchaseError />
+      ) : purchase.items ? (
         <CartAfterPurchase items={purchase.items} />
       ) : (
         <Cart cartItems={cartItems.value} onPurchase={purchase.callback} />
---
+++ interaction/components/PurchaseError.tsx
@@ -0,0 +1,8 @@
+export default function PurchaseError() {
+  return (
+    <>
+      <p>{`We're really sorry.`}</p>
+      <p>{`An unexpected error has occurred while purchasing.`}</p>
+    </>
+  );
+}
