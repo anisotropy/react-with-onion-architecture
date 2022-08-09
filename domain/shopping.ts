dcb69a9e4571b782c162bc5a5c87d11e2c741af3
anisotropy <j.hodoug@aroundus.com>
2022-08-09 12:09:18 +0900

    Display shopping items

--- domain/shopping.ts
+++ domain/shopping.ts
@@ -2,5 +2,7 @@ export {
   type ShoppingItems,
   type ShoppingItem,
   addShoppingItem,
+  mapShoppingItems,
+  readShoppingItem,
   emptyShoppingItems,
 } from "library/shopping";
--- interaction/ShoppingPage.tsx
+++ interaction/ShoppingPage.tsx
@@ -1,19 +1,19 @@
-import { addToCart } from "domain/cart";
-import { Shopping } from "./components/Shopping";
+import { mapShoppingItems, readShoppingItem } from "domain/shopping";
 import { useShoppingItems } from "./hooks/useShoppingItems";
-import { useCartState } from "./hooks/layer/useCart";
 
 export default function ShoppingPage() {
   const { shoppingItems, isLoading, isError } = useShoppingItems();
-  const cart = useCartState();
 
   if (isError) return <div>Failed to load</div>;
   if (isLoading) return <div>Loading...</div>;
-  if (!shoppingItems) return null;
 
-  const onAdd = (id: number) => {
-    cart.set((prevCart) => addToCart(prevCart, shoppingItems[id]));
-  };
-
-  return <Shopping shoppingItems={shoppingItems} onAdd={onAdd} />;
+  return (
+    <div>
+      {mapShoppingItems(shoppingItems, (item) => (
+        <div key={readShoppingItem(item, "id")}>
+          {readShoppingItem(item, "name")}
+        </div>
+      ))}
+    </div>
+  );
 }
