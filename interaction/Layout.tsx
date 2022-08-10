41d19c061dd22774c2d792654c33c9fdc9fec02e
anisotropy <j.hodoug@aroundus.com>
2022-08-10 13:04:03 +0900

    Layout style

--- interaction/Layout.tsx
+++ interaction/Layout.tsx
@@ -1,22 +1,27 @@
 import React from "react";
 import { RecoilRoot } from "recoil";
-import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 type LayoutProps = { children: React.ReactNode };
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <RecoilRoot>
-      <Navbar />
-      <main>{children}</main>
-      <Footer />
+    <>
+      <RecoilRoot>
+        <div className="wrapper">
+          <Navbar />
+          <main>{children}</main>
+        </div>
+      </RecoilRoot>
       <style jsx>{`
+        .wrapper {
+          padding: 1rem;
+        }
         main {
           margin: 1rem;
           min-height: 200px;
         }
       `}</style>
-    </RecoilRoot>
+    </>
   );
 }
